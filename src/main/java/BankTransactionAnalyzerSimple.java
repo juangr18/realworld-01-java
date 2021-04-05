b594d85e1193b5b169052b24b771d11a6f361c3d
cbarrientos <cbarrientos@ias.com.co>
2021-04-05 09:46:39 -0500

    Added unit test

--- src/main/java/BankTransactionAnalyzerSimple.java
+++ src/main/java/BankTransactionAnalyzerSimple.java
@@ -1,21 +1,11 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
-    private static final String RESOURCES = "src/main/resources/";
 
     public static void main(String[] args) throws IOException {
         BankStatementAnalyzer analyzer = new BankStatementAnalyzer();
 
         BankStatementParser bankStatementCSVParser = new BankStatementCSVParser();
-
         String fileName = "data.csv";
 
         analyzer.analyze(fileName, bankStatementCSVParser);
